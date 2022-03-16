2af573aa519b6550b63c6d6c8eda2fd0e557c495
Anandit Vashist <anandit46@gmail.com>
2022-03-16 12:22:06 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const methodOverride=require('method-override');
+const express= require("express");
+const path=require("path");
+const mongoose=require('mongoose');
+const ejsMate=require('ejs-mate');
+
+mongoose.connect('mongodb://localhost:27017/trackii',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+
+const db = mongoose.connection;
+db.on("error",console.error.bind(console,'connection error:'));
+db.once("open",()=>{
+    console.log("DB Connected");
+});
+
+const app=express()
+
+app.engine('ejs',ejsMate)
+app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
+
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"));
+
+app.get('/',(req,res)=>{
+    res.send('Connected')
+})
+
+app.listen('4000',()=>{
+    console.log('Serving on port 4000')
+})
