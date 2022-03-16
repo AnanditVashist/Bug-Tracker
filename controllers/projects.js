2068b2b659824eb4a317193727a3715243e3997c
Anandit Vashist <Anandit Vashist>
2022-03-16 13:53:43 -0700

    add index and create for project

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const mongoose=require('mongoose')
+const project = require('../models/project')
+const Project=require('../models/project')
 
-router.get('/',(req,res)=>{
-    res.send('hi from home project')
+
+router.get('/',async (req,res)=>{
+    const projects=await Project.find()
+    res.render('projects/index',{projects})
 })
+
+router.get('/create',(req,res)=>{
+    res.render('projects/create')
+})
+
+router.post('/create',async (req,res)=>{
+    const project=new Project(req.body.project);
+    await project.save();
+    res.redirect('/projects')
+
+})
+
+router.get('/details/:id',(req,res)=>{
+    res.send('hi from details')
+})
+
+
+
 
 module.exports=router
