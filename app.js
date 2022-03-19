049f8ac7d8606a5e7f37888c2699ce566a7c6294
Anandit Vashist <Anandit Vashist>
2022-03-19 12:27:41 -0700

    add session

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const methodOverride=require('method-override');
 const express= require("express");
 const path=require("path");
 const mongoose=require('mongoose');
+const passport=require('passport');
+const LocalStrategy=require('passport-local');
 const ejsMate=require('ejs-mate');
 const Project=require('./models/project')
-
+const session=require('express-session')
 
 const projectsController=require('./controllers/projects')
 const ticketsController=require('./controllers/tickets')
@@ -23,14 +25,29 @@ db.once("open",()=>{
 });
 
 const app=express()
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"));
 
+const sessionConfig={
+    secret:'thisShouldBeABetterSecret',
+    resave:false,
+    saveUnitialized:true,
+    cooke:{
+        httpOnly: true,
+        expires: Date.now()+1000*60*60*24*7,
+        maxAge: 1000*60*60*24*7
+    }
+}
 app.engine('ejs',ejsMate)
 app.use(express.urlencoded({ extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static('assets'))
+app.use(session(sessionConfig))
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.set("view engine","ejs");
-app.set("views", path.join(__dirname,"views"));
+
+
 
 app.use('/projects', projectsController)
 app.use('/tickets', ticketsController)
@@ -39,9 +56,6 @@ app.use('/tickets', ticketsController)
 app.get('/',(req,res)=>{
     res.send('Connected')
 })
-
-
-
 
 app.listen('4000',()=>{
     console.log('Serving on port 4000')
