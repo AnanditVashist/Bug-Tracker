9466589712136e0cbe7c6ac75c934f689f524fd6
Anandit Vashist <Anandit Vashist>
2022-03-18 23:01:02 -0700

    add navbar

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const app=express()
 app.engine('ejs',ejsMate)
 app.use(express.urlencoded({ extended: true}));
 app.use(methodOverride('_method'));
+app.use(express.static('assets'))
 
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
