8df65ef308af4861f02b4ea5c437eab77dbecaf4
Anandit Vashist <Anandit Vashist>
2022-03-20 23:34:44 -0700

    add debug launch settings

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const LocalStrategy=require('passport-local');
 const ejsMate=require('ejs-mate');
 const Project=require('./models/project')
 const session=require('express-session')
+const User=require('./models/user')
 
 const projectsController=require('./controllers/projects')
 const ticketsController=require('./controllers/tickets')
-
+const usersController=require('./controllers/users')
 
 mongoose.connect('mongodb://localhost:27017/trackii',{
     useNewUrlParser: true,
@@ -45,18 +46,23 @@ app.use(express.static('assets'))
 app.use(session(sessionConfig))
 app.use(passport.initialize())
 app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
 
-
-
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.use('/projects', projectsController)
 app.use('/tickets', ticketsController)
+app.use('/', usersController)
 
+app.listen('4000',()=>{
+    console.log('Serving on port 4000')
+})
 
 app.get('/',(req,res)=>{
     res.send('Connected')
 })
 
-app.listen('4000',()=>{
-    console.log('Serving on port 4000')
-})
+
+
+module.exports = app;
--- controllers/projects.js
+++ controllers/projects.js
@@ -2,18 +2,21 @@ const express = require('express')
 const router = express.Router()
 const mongoose=require('mongoose')
 const Project=require('../models/project')
+const {isLoggedIn}=require('../middleware')
 
 
-router.get('/',async (req,res)=>{
+router.get('/',isLoggedIn,async (req,res)=>{
     const projects=await Project.find()
     res.render('projects/index',{projects})
 })
 
-router.get('/create',(req,res)=>{
+router.get('/create',isLoggedIn,(req,res)=>{
+  
     res.render('projects/create')
 })
 
-router.post('/create',async (req,res)=>{
+router.post('/create',isLoggedIn,async (req,res)=>{
+    
     const project=new Project(req.body.project);
     await project.save();
     res.redirect('projects')
@@ -21,23 +24,23 @@ router.post('/create',async (req,res)=>{
 })
 
 
-router.get('/details/:id',async (req,res)=>{
+router.get('/details/:id',isLoggedIn,async (req,res)=>{
     const project=await Project.findById(req.params.id)
     res.render('projects/details',{project})
 })
 
-router.get('/edit/:id',async (req,res)=>{
+router.get('/edit/:id',isLoggedIn,async (req,res)=>{
     const project=await Project.findById(req.params.id)
     res.render('projects/edit',{project})
 })
 
-router.put('/edit/:id', async (req,res)=>{
+router.put('/edit/:id',isLoggedIn, async (req,res)=>{
     const {id}=req.params;
     const project=await Project.findByIdAndUpdate(id,{...req.body.project})
     res.redirect('/projects')
 })
 
-router.delete('/delete/:id', async (req,res)=>{
+router.delete('/delete/:id',isLoggedIn, async (req,res)=>{
     const {id}=req.params;
     const project=await Project.deleteOne({_id:id})
     res.redirect('/projects')
