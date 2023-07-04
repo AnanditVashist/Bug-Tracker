845b77df7d9c48c9f392ba657f7b51bf524778df
Anandit <anandit46@gmail.com>
2023-07-04 02:23:19 -0400

    Updates

--- middleware.js
+++ middleware.js
@@ -1,46 +1,41 @@
-const {roles} = require('./utilities/roles')
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
-}
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
-module.exports.isLoggedIn=(req,res,next)=>{
-    if(!req.isAuthenticated()){
-         
-     return res.redirect('/identity/login')
-    }
-    next();
+const { roles } = require("./utilities/roles");
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
 }
 
- 
-module.exports.grantAccess = function(action, resource) {
- return async (req, res, next) => {
-  try {
-   const permission = roles.can(req.user.role)[action](resource);
-   if (!permission.granted) {
-    return res.status(401).json({
-     error: "You don't have enough permission to perform this action"
-    });
-   }
-   next()
-  } catch (error) {
-   next(error)
-  }
- }
-}
- 
-module.exports.checkForDemoUser = function(action,resource) {
-    return async (req, res, next) => {
-     try {
-      if ((req.user.role.search('Demo')) !== -1) {
-          req.flash('info',`Your ${action} ${resource} has been received!`)
-       return res.redirect('/projects')
-      }
-      next()
-     } catch (error) {
-      next(error)
-     }
-    }
-   }
+module.exports.isLoggedIn = (req, res, next) => {
+	if (!req.isAuthenticated()) {
+		return res.redirect("/identity/login");
+	}
+	next();
+};
 
+module.exports.grantAccess = function (action, resource) {
+	return async (req, res, next) => {
+		try {
+			const permission = roles.can(req.user.role)[action](resource);
+			if (!permission.granted) {
+				return res.status(401).json({
+					error: "You don't have enough permission to perform this action",
+				});
+			}
+			next();
+		} catch (error) {
+			next(error);
+		}
+	};
+};
+
+module.exports.checkForDemoUser = function (action, resource) {
+	return async (req, res, next) => {
+		try {
+			if (req.user.role.search("Demo") !== -1) {
+				req.flash("info", `Your ${action} ${resource} has been received!`);
+				return res.redirect("/projects");
+			}
+			next();
+		} catch (error) {
+			next(error);
+		}
+	};
+};
